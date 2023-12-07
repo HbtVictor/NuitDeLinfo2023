27b36aafaf01b14e4281f6c335f2a7800020301c
HbtVictor <=>
2023-12-07 23:28:24 +0100

    commit

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FirstController extends AbstractController
+class AccueilController extends AbstractController
 {
-    #[Route('/blog/accueil', name: 'app_first')]
+    #[Route('/blog/accueil', name: 'accueil')]
     public function index(): Response
     {
         return $this->render('first/index.html.twig', [
