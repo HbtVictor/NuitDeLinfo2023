55fdbfe664b3f8b43cb05543f1293a8bfc9677ec
HbtVictor <=>
2023-12-07 22:17:47 +0100

    commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/conexion', name: 'security.login', methods: ['GET', 'POST'])]
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
     public function index(): Response
     {
         return $this->render('security/login.html.twig', [
