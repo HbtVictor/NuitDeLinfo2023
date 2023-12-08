16e9f94c427ba7024011b08ecd1e7188c5c40565
HbtVictor <=>
2023-12-08 04:01:17 +0100

    commit

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\DebugBundle;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
