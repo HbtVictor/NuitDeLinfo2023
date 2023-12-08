7a74655f5cefa76f735eaf55b5fded4200a339c7
HbtVictor <=>
2023-12-08 05:17:08 +0100

    commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
+
+
+
+    #[Route('/utilisateur', name: 'user')]
+    public function userInterface(User $user): Response
+    {
+
+        if(!$this->getUser()){
+            return $this->redirectToRoute('security.login');
+        }
+
+        if($this->getUser() !== $user){
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('user/user.html.twig',[
+            'userName' => $user->getPseudo(),
+            'userEmail' => $user->getEmail(),
+            'userPassword' => $user->getPassword()
+        ]);
+    }
+
+
+
     #[Route('/utilisateur/edit/{id}', name: 'user.edit')]
     public function index(User $user): Response
     {
@@ -21,7 +45,7 @@ class UserController extends AbstractController
         if($this->getUser() !== $user){
             return $this->redirectToRoute('accueil');
         }
-        
+
         $form = $this->createForm(UserType::class, $user);
 
         return $this->render('user/edit.html.twig', [
