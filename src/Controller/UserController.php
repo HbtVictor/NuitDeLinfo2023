0eda38237a0b3f459c84ec8c778978ae80489990
HbtVictor <=>
2023-12-08 05:08:31 +0100

    commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,7 +21,7 @@ class UserController extends AbstractController
         if($this->getUser() !== $user){
             return $this->redirectToRoute('accueil');
         }
-
+        
         $form = $this->createForm(UserType::class, $user);
 
         return $this->render('user/edit.html.twig', [
